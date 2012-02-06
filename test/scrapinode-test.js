fe787bf171f94f6c7a2bb9e1d021998e72def678
Remy Loubradou <remyloubradou@gmail.com>
2012-02-06 17:17:19 +0100

    NEW: Add two tests.

--- test/scrapinode-test.js
+++ test/scrapinode-test.js
@@ -13,6 +13,8 @@ var suite = vows.describe('Scrapinode');
 var pathScrapulesDir = __dirname + '/scrapules/';
 var urlDefault = 'http://www.comet.co.uk/p/Plasma-TVs/buy-LG-42PT353-Plasma-TV/721050';
 var urlCultBeauty = 'http://www.cultbeauty.co.uk/clarisonic/clarisonic-plus_sonic_skin_cleansing_system.php';
+var urlImage = 'http://ecx.images-amazon.com/images/I/51RRbTkbfBL._SL500_AA300_.jpg';
+var urlwithHTTPRefreshMeta = 'http://www.windowsphone.com/en-US/apps/82a23635-5bd9-df11-a844-00237de2db9e';
 
 suite.addBatch({
    'Load a scrapule in scrapinode' : {
@@ -69,6 +71,37 @@ suite.addBatch({
          },
          'should give a Scraper object able to retrieve the price of the product' : function(err,scraper){
             assert.match(scraper.get('price'),/([0-9,]{1,}(\.?[0-9]{1,}))/)
+         }
+      },
+      'to scrap an HTML page created because the resource found with the link given is an image' : {
+         topic : function(){
+            scrapinode.createScraper(urlImage,this.callback)
+         },
+         'should happen without error' : function(err,scraper){
+            assert.isNull(err);
+         },
+         'should give a Scraper object' : function(err,scraper){
+            assert.instanceOf(scraper,Scraper);
+         },
+         'should give a Scraper object able to retrieve images of the page' : function(err,scraper){
+            assert.isArray(scraper.get('images'));
+         },
+      },
+      'to scrap an HTML page with in it a HTTP-REFRESH meta tag' : {
+         topic : function(){
+            scrapinode.createScraper(urlwithHTTPRefreshMeta,this.callback)
+         },
+         'should give a Scraper object' : function(err,scraper){
+            assert.instanceOf(scraper,Scraper);
+         },
+         'should give a Scraper object able to retrieve the title of the page' : function(err,scraper){
+            assert.isString(scraper.get('title'));
+         },
+         'should give a Scraper object able to retrieve a description of the page' : function(err,scraper){
+            assert.isArray(scraper.get('description'));
+         },
+         'should give a Scraper object able to retrieve images of the page' : function(err,scraper){
+            assert.isArray(scraper.get('images'));
          }
       }
    }
