b0ae6b11369212e5ddd492431c87b92f282310b5
Remy Loubradou <remyloubradou@gmail.com>
2013-06-06 19:55:16 +0100

    NEW: Add test to check unzip stream error are correctly handled.
    #30

---
+++ test/unzip-error-test.js
@@ -0,0 +1,26 @@
+/**
+ * Modules dependencies
+ */
+
+var mocha = require('mocha'),
+	assert = require('chai').assert,
+	libPath = process.env['SCRAPINODE_COV'] ? '../lib-cov' : '../lib',
+	scrapinode = require( libPath + '/scrapinode'),
+	ScrapinodeError = require(libPath + '/error/scrapinode-error'),
+	express = require('express'),
+	fs = require('fs');
+
+// Test suite
+
+describe('scrapinode#createScraper(url,callback)',function(){
+	describe('when corrupted zipped data is retrieved',function(){
+		it('should return an error Object in the callback as first argument',function(done){
+			scrapinode.useAll(scrapinode.defaults());
+			scrapinode.createScraper('http://www.cavagogo.com/index.cfm',function(err,scraper){
+				assert.instanceOf(err,Error);
+				assert.isUndefined(scraper);
+				done();
+			})
+		});
+	});
+});
